5e42faa3d3d03ef3ecdcbef946f95fdaee9a3b06
Jorengarenar <dev@joren.ga>
2021-07-24 12:41:33 +0200

    Proper function names

--- script.js
+++ script.js
@@ -5,53 +5,53 @@ const patterns = {
   "playlist": /^.*?youtube\.com\/(?:playlist\?|watch\?v=.+?&)list=(.+?)(?:&index=\d+|$)/,
 };
 
-async function bar(url) {
+async function getChannelId(url) {
   const response = await fetch(`https://api.codetabs.com/v1/proxy?quest=${url}`);
   const text = await response.text();
   let id = text.match(/<meta itemprop="channelId" content="(.+?)">/)[1];
   return { "type": "channel", "id": id };
 }
 
-async function foo(url) {
+async function getIdOfPlaylistOrChannel(url) {
   try {
     return { "type": "playlist", "id": url.match(patterns.playlist)[1] };
   } catch (e) {
-    return bar(url);
+    return getChannelId(url);
   }
 }
 
-async function baz(name) {
-  const x = async (t) => {
-    try {
-      return await bar(`https://www.youtube.com/${t}/${name}`);
-    } catch(e) {
-      return null;
-    }
-  };
+async function getIdByChannelName(name) {
+  const p = (t) => new Promise((resolve) => resolve(
+    async function() {
+      try {
+        return await getChannelId(`https://www.youtube.com/${t}/${name}`);
+      } catch(e) {
+        return null;
+      }
+    }()
+  ));
 
-  const xy = (t) => new Promise((resolve) => resolve(x(t)));
-
-  const responses = await Promise.all([xy("c"), xy("user"), xy("channel")]);
+  const responses = await Promise.all([ p("c"), p("user"), p("channel") ]);
 
   for (let r of responses) {
     if (r) { return r; }
   }
 }
 
-async function bax(url) {
+async function getId(input) {
   try {
-    url = `http://www.youtube.com/${url.match(patterns.validate)[1]}`;
-    return await foo(url);
+    url = `http://www.youtube.com/${input.match(patterns.validate)[1]}`;
+    return await getIdOfPlaylistOrChannel(url);
   } catch (e) {
-    return await baz(url);
+    return await getIdByChannelName(input);
   }
 }
 
-async function generate() {
+async function getRss() {
   rss.textContent = "...";
-  let url = document.querySelector("#url").value.trim();
+  let input = document.querySelector("#url").value.trim();
   try {
-    const { type, id } = await bax(url);
+    const { type, id } = await getId(input);
     rss.textContent = `https://www.youtube.com/feeds/videos.xml?${type}_id=${id}`;
   } catch (e) {
     rss.textContent = "Invalid input";
